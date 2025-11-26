d350eb3b6bdb686845fbc861e91317f7500b281b
Stefan Stoller <ste.stoller@gmail.com>
2025-11-26 18:37:29 +0100

    added localsend firewall rules

--- configs.sh
+++ configs.sh
@@ -2,4 +2,12 @@
 
 # exchange configs
 sudo cp ~/Projects/omarchy-supplement/files/hosts /etc/hosts
+echo "exchanged hosts file"
+
 cp ~/Projects/omarchy-supplement/scripts/* ~/Scripts/
+echo "copied custom scripts"
+
+# open firewall for localsend
+sudo ufw allow 53317/tcp
+sudo ufw allow 53317/udp
+echo "fireall adjusted for localsend"
